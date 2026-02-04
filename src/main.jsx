986460e53d5ae90ee57bbcd83bed049c14a5cd94
Ekyhy <eksayahya92@gmail.com>
2026-02-04 15:04:49 +0700

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './dist/css/main.css';
+import './styles/main.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
